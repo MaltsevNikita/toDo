e99075500977acec68fb2ac6c3bb3c5a71d02663
unknown <mn121288@gmail.com>
2019-10-12 15:31:08 +0400

    First commit

---
+++ js.js
@@ -0,0 +1,35 @@
+window.onload = function (){
+
+	var todoList = [];
+	document.getElementById('add').onclick = function(){
+		var d = document.getElementById('in').value;
+		var desc = document.getElementById('desc').value;
+
+		/*
+		 {todo: Добавить хлеб, check: false, desc: Описание}
+		*/
+		var temp = {};
+		temp.todo = d;
+		temp.desc = desc;
+		temp.check = false;
+		var i = todoList.length;
+		todoList[i] = temp;
+		//todoListd[i] = tempd;
+		console.log(todoList);
+		out();
+	}
+
+	function out(){
+		var out = '';
+		for (var key in todoList){
+			if(todoList[key].check == true){
+				out += '<input type="checkbox" checked>';
+			}
+			else {
+				out += '<input type="checkbox">';
+			}
+			out += todoList[key].todo + '<br>' + todoList[key].desc + '<br>' + '<br>';
+		}
+		document.getElementById('out').innerHTML = out;
+	}
+}
